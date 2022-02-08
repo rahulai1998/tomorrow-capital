ef0866706bcfb28996c299d9a42494fc8d0f72e7
Rahul Baban Chavan <48827606+rahulai1998@users.noreply.github.com>
2022-02-08 18:49:08 +0530

    Update index.php
    added email demo fun

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php 
  include_once("index.html");
+ $to_email = "rahulchavan.rc1998@gmail.com";
+   $subject = "Simple Email Test via PHP";
+   $body = "Hi, This is test email send by PHP Script";
+   $headers = "From: kamblepramod806@gmail.com";
+
+   if (mail($to_email, $subject, $body, $headers)) {
+      echo "Email successfully sent to $to_email...";
+   } else {
+      echo "Email sending failed...";
+   }
 ?>
